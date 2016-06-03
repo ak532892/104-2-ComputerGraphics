ca56d3b35abcbc8bea64df19c8c43c990639a9f7
ak532892 <ak532892@gmail.com>
2016-06-03 23:58:51 +0800

    comments

--- hw/hw7/hw7.js
+++ hw/hw7/hw7.js
@@ -3,13 +3,13 @@ var pointLight;
 var scene, sceneMono, sceneColor;
 var camera2;
 var rtTexture;
-var torus;
+var torus, skybox;
 var gcontrols;
 
 // blocker pass
 // 1st pass: torus (colorWrite off)
 // 2nd pass: column
-
+// https://clara.io/view/1c0c6a1d-bac1-4c89-8baf-b0c04355a611
 
 // 
 // render sceneMono to rtTexture (camera)
@@ -22,7 +22,6 @@ animate();
 
 function init()
 {
-	
 	rtTexture = new THREE.WebGLRenderTarget( 
 		1024, 1024, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat 
 	});
@@ -42,14 +41,14 @@ function init()
 	///////////////////////////////////////////////////////////////////////		
 	sceneMono = new THREE.Scene();	
 	pointLight = new THREE.PointLight(0xffffff, 1.0);
-	pointLight.position.set(0, 300, 500);
+	pointLight.position.set(0, 100, 500);
 	sceneMono.add (pointLight);
 
 	torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 16, 100),
 	new THREE.MeshLambertMaterial({color:0x12ffee, 
 		colorWrite:false
 	}));
-	torus.scale.set(3, 3, 3);
+	torus.scale.set(15, 15, 15);
 	torus.rotation.x = Math.PI/2;
 	sceneMono.add(torus);
 	
@@ -61,18 +60,38 @@ function init()
 	
 	sceneColor = new THREE.Scene();
 	THREE.ImageUtils.crossOrigin = '';
+	
+	var material = new THREE.MeshLambertMaterial({
+		side: THREE.BackSide
+	});
+	skybox = new THREE.Mesh( new THREE.BoxGeometry( 500, 500, 500 ), material );
+	sceneMono.add (skybox);
+	
+	var bumpMap = THREE.TextureLoader('https://ak532892.github.io/ComputerGraphics2016/hw/hw7/models/dragon_ N.jpg');
 	var loader = new THREE.ObjectLoader();
+	loader.crossOrigin = '';
+	loader.setCrossOrigin('');
 	loader.load (
-		'http://ak532892.github.io/ComputerGraphics2016/hw/hw7/models/dragon.jason', 
+		'https://ak532892.github.io/ComputerGraphics2016/hw/hw7/models/dragon.json', 
 		function ( obj ) {
-			obj.scale.set (3, 3, 3);
+			obj.scale.set (12, 12, 12);
+			obj.rotation.y = Math.PI;
+			obj.traverse (
+				function (mesh) {
+					if (mesh instanceof THREE.Mesh) {
+						mesh.material.bumpMap = bumpMap;
+						mesh.material.bumpScale = 0.2;
+					}
+				}
+			);
 			sceneColor.add( obj );
 		}
 	);
+	//https://clara.io/view/1c0c6a1d-bac1-4c89-8baf-b0c04355a611
 	
 	// same light cannot be added to two scenes
 	var pointLight2 = new THREE.PointLight(0xffffff, 1.0);
-	pointLight2.position.set(0, 300, 500);
+	pointLight2.position.set(0, 100, 500);
 	sceneColor.add(pointLight2);
 
 	scene = new THREE.Scene();
@@ -109,13 +128,15 @@ function animate()
 	
 	renderer.setClearColor (0x888800);
 	torus.material.colorWrite = true;
-
+	skybox.material.colorWrite = true;
+	
 	//color buffer
 	renderer.render (sceneMono, camera, rtTexture, true);
 	renderer.setClearColor (0x888888);
 	renderer.render (scene, camera2);
 	
 	torus.material.colorWrite = false;
+	skybox.material.colorWrite = false;
 	//depth buffer
 	renderer.render (sceneMono, camera);
 	renderer.render (sceneColor, camera);
